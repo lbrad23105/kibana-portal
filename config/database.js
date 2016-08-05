4604b30ff49c06b56b67c1c543c82248c6c65272
Luke Brady <lbrad23105@gmail.com>
2016-08-05 01:34:21 -0400

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 // config/database.js
 module.exports = {
 
-    'url' : '192.168.99.100:32777' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+    'url' : '192.168.99.100:32777'
 
 };
